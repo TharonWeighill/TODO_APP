5ee26661f221224916aad6ecf9626f4baa1ff223
tharonweighill <tharon@fusionauth.io>
2022-04-25 12:24:22 -0600

    ADD todoService ADD todoServiceImpl

--- src/main/java/com/example/todo_app/Service/TodoService.java
+++ src/main/java/com/example/todo_app/Service/TodoService.java
@@ -1,7 +1,8 @@
 package com.example.todo_app.Service;
 
+import com.example.todo_app.Models.TodoLists;
 import org.springframework.stereotype.Service;
-
 @Service
-public class TodoService {
+public interface TodoService {
+    TodoLists savedTodos(TodoLists todoLists);
 }
---
+++ src/main/java/com/example/todo_app/Service/TodoServiceImpl.java
@@ -0,0 +1,11 @@
+package com.example.todo_app.Service;
+
+import com.example.todo_app.Models.TodoLists;
+import org.springframework.stereotype.Service;
+@Service
+public class TodoServiceImpl implements TodoService {
+    @Override
+    public TodoLists savedTodos(TodoLists todoLists) {
+        return null;
+    }
+}
