ceb88e76e35d185779ca4b4cf3dfefd62c352082
tharonweighill <tharon@fusionauth.io>
2022-04-25 13:22:15 -0600

    REFRACTOR controller

---
+++ src/main/java/com/example/todo_app/Controller/TodoController.java
@@ -0,0 +1,21 @@
+package com.example.todo_app.Controller;
+
+import com.example.todo_app.Models.TodoLists;
+import com.example.todo_app.Service.TodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/lists")
+public class TodoController {
+    private TodoService todoService;
+    public TodoController( TodoService todoService ) {
+        super();
+        this.todoService = todoService;
+    }
+    @PostMapping
+    public ResponseEntity<TodoLists> saveTodoLists(@RequestBody TodoLists todoLists){
+        return new ResponseEntity<TodoLists>(todoService.savedTodos(todoLists), HttpStatus.CREATED);
+    }
+}
--- src/main/java/com/example/todo_app/Exception/ResourceNotFound.java
+++ src/main/java/com/example/todo_app/Exception/ResourceNotFound.java
@@ -2,9 +2,12 @@ package com.example.todo_app.Exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.io.Serial;
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFound extends RuntimeException{
 
+    @Serial
     private static final long serialVersionUID =1L;
     private String resourceName;
     private String fieldName;
