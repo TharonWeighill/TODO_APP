73d646efaf692ac811505c2467a0ecf61a931abf
tharonweighill <tharon@fusionauth.io>
2022-05-04 07:43:38 -0600

    ADD exchange oauth code for access token

--- src/main/java/com/example/todo_app/Controller/FusionAuthController.java
+++ src/main/java/com/example/todo_app/Controller/FusionAuthController.java
@@ -1,18 +1,19 @@
 package com.example.todo_app.Controller;
-import com.example.todo_app.Models.TodoLists;
-import com.example.todo_app.Service.TodoService;
-import org.springframework.data.jpa.repository.Query;
+import io.fusionauth.client.FusionAuthClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
 @RestController
 @CrossOrigin
 @RequestMapping ("/oauth-redirect")
 public class FusionAuthController {
+
+    private String apiKey = "9b26f51e-6b0e-4d33-93a0-619d45fa3923";
+    private String clientId = "a7f398aa-8337-4112-94a0-f99bc29a8136";
+    private String clientSecret = "nTUHnlCcKvVnVhofP47ROBaWulWbK84N-l-xc3PzC2g";
+    private String  fusionAuthURL = "http://localhost:9011";
+    FusionAuthClient client = new FusionAuthClient(apiKey, "http://localhost:9011");
+
 
     public FusionAuthController() {
 
@@ -26,11 +27,11 @@ public class FusionAuthController {
         //8) connect cleanspeak to post todos
 
     }
-
-    @GetMapping()
-    @CrossOrigin
-    public ResponseEntity<Void> getAuthCode(@RequestParam("code") String authCode) {
-        System.out.println(authCode);
-        return new ResponseEntity<Void>(HttpStatus.OK);
-    }
+        @GetMapping()
+        @CrossOrigin
+        public ResponseEntity<Void> getAuthCode (@RequestParam("code") String authCode){
+        client.exchangeOAuthCodeForAccessToken(authCode, clientId, clientSecret, "http://localhost:9900/oauth-redirect");
+            System.out.println(authCode);
+            return new ResponseEntity<Void>(HttpStatus.OK);
+        }
 }
